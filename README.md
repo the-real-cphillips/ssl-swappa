3f2947a8a8cf88c71e1da25ba25b2f8db09e31ab
Craig Phillips <cphillips@2u.com>
2018-05-29 08:43:07 -0400

    Inital commit for SSL SWAPPA

---
+++ README.md
@@ -0,0 +1,42 @@
+# ACM SSL Swap for AWS ELB's
+
+### Usage
+
+```
+usage: sslswappa.py [-h] [-r REGION] [-p PORT] -a ACCOUNT_NUMBER -c CERT_ID
+                       -e ELB_NAME
+sslswappa.py: error: argument -a/--account_number is required
+```
+
+### Setup
+
+1. Clone this
+1. Create a virtualenv `virtualenv sslswappa`
+1. Activate the virtualenv: `source sslswappa/bin/activate`
+1. Install the requirements: `pip install -r requirements.txt`
+1. Run script with your parameters
+
+### Params
+- `-r` - Region ie us-west-2, us-east-1, etc.
+- `-p` - Port number, defaults to `443`
+- `-a` - [REQUIRED] AWS Account Number
+- `-c` - [REQUIRED] ACM Certificate ID, Just the ID not the full ARN
+- `-e` - [REQUIRED] ELB Name, the actual NAME not the FQDN
+
+### Example
+
+```
+./sslswappa.py -r us-west-2 -a xxxxxxxxxxxx -c xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx -e my-elb-test
+```
+
+Success Output:
+
+```
+[O] Success! ELB: cphillips-mktg-test's SSL Cert was Updated
+```
+
+Failure Output:
+
+```
+[x] Something went wrong, please check your values again
+```
---
+++ sslswappa.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+import argparse
+import boto3
+import sys
+from termcolor import colored, cprint
+
+
+parser = argparse.ArgumentParser(description="Swap ACM Certs on an ELB")
+
+parser.add_argument('-r', '--region',
+                    dest='region',
+                    action='store',
+                    default='us-west-2',
+                    help="Specify region, default is us-west-2")
+parser.add_argument('-p', '--port',
+                    dest='port',
+                    action='store',
+                    default=443,
+                    type=int,
+                    help="Specify Listener port, default is 443")
+parser.add_argument('-a', '--account_number',
+                    dest='account_number',
+                    action='store',
+                    required=True,
+                    help="AWS Account Number")
+parser.add_argument('-c', '--cert_id',
+                    dest='cert_id',
+                    action='store',
+                    required=True,
+                    help="ACM Certificate ID")
+parser.add_argument('-e', '--elb_name',
+                    dest='elb_name',
+                    action='store',
+                    required=True,
+                    help="AWS ELB Name")
+
+args = parser.parse_args()
+
+client = boto3.client('elb', region_name=args.region)
+
+def swap_ssl(elb_name, port, account_number, cert_id, region):
+    """ Swap SSL Certs on ELBs, specific usage for ACM Certs """
+    try:
+        response = client.set_load_balancer_listener_ssl_certificate(
+            LoadBalancerName=elb_name,
+            LoadBalancerPort=port,
+            SSLCertificateId="arn:aws:acm:%s:%s:certificate/%s" % (region, account_number, cert_id)
+            )
+        return response
+    except client.exceptions.AccessPointNotFoundException as error_message:
+        return error_message
+        
+
+def parse_response(response):
+    try:
+        response = response['ResponseMetadata']['HTTPStatusCode']
+    except TypeError:
+        cprint("[x] Something went wrong, please check your values again", 'red')
+
+    if type(response) is int:
+        if response == 200:
+            cprint("[O] Success! ELB: %s's SSL Cert was Updated" % (args.elb_name), 'green')
+
+
+def main():
+    response = swap_ssl(args.elb_name, args.port, args.account_number, args.cert_id, args.region)
+    parse_response(response)
+
+
+if __name__ == '__main__':
+    main()
